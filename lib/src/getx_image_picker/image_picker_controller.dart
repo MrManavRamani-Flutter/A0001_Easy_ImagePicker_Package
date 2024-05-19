252d5071a497009c5b8bbcb09adaeffeb2cde2dc
manav <manavpatel13402003@gmail.com>
2024-05-19 18:56:43 +0530

    code in changes in GetX

--- lib/src/getx_image_picker/image_picker_controller.dart
+++ lib/src/getx_image_picker/image_picker_controller.dart
@@ -1,17 +1,14 @@
-// lib/image_picker_controller.dart
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
-import 'image_model.dart';
-
 class ImagePickerController extends GetxController {
   final ImagePicker _picker = ImagePicker();
-  var selectedImage = Rxn<ImageModel>();
+  var selectedImagePath = ''.obs;
 
   Future<void> pickImage(ImageSource source) async {
     final XFile? image = await _picker.pickImage(source: source);
     if (image != null) {
-      selectedImage.value = ImageModel.fromXFile(image);
+      selectedImagePath.value = image.path;
     } else {
       Get.snackbar('Error', 'No image selected');
     }
