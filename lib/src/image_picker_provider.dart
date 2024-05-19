e104efd7e4f5f447da55195da43753253f2a7ba0
manav <manavpatel13402003@gmail.com>
2024-05-19 14:17:42 +0530

    change code in provider

--- lib/src/image_picker_provider.dart
+++ lib/src/image_picker_provider.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-import 'image_model.dart'; // Import the ImageModel class
+import '../a0001_easy_image_picker.dart';
 
 class ImagePickerProvider extends ChangeNotifier {
   ImageModel? _selectedImage;
 
   ImageModel? get selectedImage => _selectedImage;
 
-  void pickImage() async {
+  Future<void> pickImage() async {
     final picker = ImagePicker();
-    final XFile? pickedFile =
-        await picker.pickImage(source: ImageSource.gallery);
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       _selectedImage = ImageModel.fromXFile(pickedFile);
-      notifyListeners();
+      notifyListeners(); // Notify listeners when the image is picked
     }
   }
 }
