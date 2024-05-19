ecf6d6dbe26af67ad0f05aed63a19d5c6ac17e95
manav <manavpatel13402003@gmail.com>
2024-05-19 13:14:20 +0530

    1st provider code rewrite

--- lib/src/image_picker_controller.dart
+++ lib/src/image_picker_controller.dart
@@ -1,16 +1,1 @@
-import 'package:get/get.dart';
-import 'package:image_picker/image_picker.dart';
 
-import 'image_model.dart';
-
-class ImagePickerController extends GetxController {
-  var imageModel = Rx<ImageModel?>(null);
-
-  Future<void> pickImage(ImageSource source) async {
-    final pickedFile = await ImagePicker().pickImage(source: source);
-
-    if (pickedFile != null) {
-      imageModel.value = ImageModel.fromXFile(pickedFile);
-    }
-  }
-}
--- lib/src/image_picker_provider.dart
+++ lib/src/image_picker_provider.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-import 'image_model.dart';
+import 'image_model.dart'; // Import the ImageModel class
 
 class ImagePickerProvider extends ChangeNotifier {
-  ImageModel? _imageModel;
+  ImageModel? _selectedImage;
 
-  ImageModel? get imageModel => _imageModel;
+  ImageModel? get selectedImage => _selectedImage;
 
-  Future<void> pickImage(ImageSource source) async {
-    final pickedFile = await ImagePicker().pickImage(source: source);
-
+  void pickImage() async {
+    final picker = ImagePicker();
+    final XFile? pickedFile =
+        await picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
-      _imageModel = ImageModel.fromXFile(pickedFile);
+      _selectedImage = ImageModel.fromXFile(pickedFile);
       notifyListeners();
     }
   }
