38bd15a8f6021bea372d712e80bca8245e80ff02
manav <manavpatel13402003@gmail.com>
2024-05-21 19:40:42 +0530

    change design snackbar with ui/ux design....

--- lib/src/getx_image_picker/image_picker_controller.dart
+++ lib/src/getx_image_picker/image_picker_controller.dart
@@ -12,11 +12,53 @@ class ImagePickerController extends GetxController {
     final XFile? image = await _picker.pickImage(source: source);
     if (image != null) {
       selectedImagePath.value = image.path;
-      Get.snackbar('Success', 'image selected',
-          backgroundColor: Colors.green, snackPosition: SnackPosition.BOTTOM);
+      Get.snackbar(
+        'Success',
+        'Image selected',
+        backgroundColor: Colors.green,
+        colorText: Colors.white,
+        snackPosition: SnackPosition.BOTTOM,
+        margin: const EdgeInsets.all(10),
+        borderRadius: 8,
+        icon: const Icon(Icons.check_circle, color: Colors.white),
+        mainButton: TextButton(
+          onPressed: () {
+            Get.back(); // Dismisses the snackbar
+          },
+          child: const Text(
+            'CLOSE',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      );
     } else {
-      Get.snackbar('Error', 'No image selected',
-          backgroundColor: Colors.red, snackPosition: SnackPosition.BOTTOM);
+      Get.snackbar(
+        'Error',
+        'No image selected',
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        snackPosition: SnackPosition.BOTTOM,
+        margin: const EdgeInsets.all(10),
+        borderRadius: 8,
+        icon: const Icon(Icons.error, color: Colors.white),
+        mainButton: TextButton(
+          onPressed: () {
+            Get.back(); // Dismisses the snackbar
+          },
+          child: const Text(
+            'CLOSE',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      );
     }
+    // if (image != null) {
+    //   selectedImagePath.value = image.path;
+    //   Get.snackbar('Success', 'image selected',
+    //       backgroundColor: Colors.green, snackPosition: SnackPosition.BOTTOM);
+    // } else {
+    //   Get.snackbar('Error', 'No image selected',
+    //       backgroundColor: Colors.red, snackPosition: SnackPosition.BOTTOM);
+    // }
   }
 }
