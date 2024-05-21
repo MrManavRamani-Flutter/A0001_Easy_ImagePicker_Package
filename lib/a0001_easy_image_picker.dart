67f650760276f3969dbdd7d1c2c1d36a212bd470
manav <manavpatel13402003@gmail.com>
2024-05-21 20:56:40 +0530

    bloc code upload first for test code...

--- lib/a0001_easy_image_picker.dart
+++ lib/a0001_easy_image_picker.dart
@@ -2,7 +2,7 @@
 
 library a0001_easy_image_picker;
 
-export 'src/bloc_image_picker/bloc/image_picker_bloc.dart';
+export 'src/bloc_image_picker/image_picker_bloc.dart';
 export 'src/getx_image_picker/image_picker_controller.dart';
 export 'src/provider_image_picker/image_model.dart';
 export 'src/provider_image_picker/image_picker_provider.dart';
