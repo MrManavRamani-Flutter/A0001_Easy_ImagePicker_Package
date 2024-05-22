2e849bcdbbba922e5052bbafae2ecf23061decfd
manav <manavpatel13402003@gmail.com>
2024-05-22 08:09:35 +0530

    error or success Message

--- lib/src/bloc_image_picker/image_picker_bloc.dart
+++ lib/src/bloc_image_picker/image_picker_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
@@ -21,15 +22,23 @@ class ImagePickerBloc extends Bloc<ImagePickerEvent, ImagePickerState> {
       final pickedFile = await _imagePicker.pickImage(source: event.source);
       if (pickedFile != null) {
         emit(ImagePickerLoaded(File(pickedFile.path)));
+        Fluttertoast.showToast(
+            msg: "image selected",
+            toastLength: Toast.LENGTH_SHORT,
+            backgroundColor: Colors.green);
       } else {
         emit(ImagePickerError("No image selected"));
         Fluttertoast.showToast(
-            msg: "No image selected", toastLength: Toast.LENGTH_SHORT);
+            msg: "No image selected",
+            toastLength: Toast.LENGTH_SHORT,
+            backgroundColor: Colors.redAccent);
       }
     } catch (e) {
       emit(ImagePickerError(e.toString()));
       Fluttertoast.showToast(
-          msg: "Error: ${e.toString()}", toastLength: Toast.LENGTH_SHORT);
+          msg: "Error: ${e.toString()}",
+          toastLength: Toast.LENGTH_SHORT,
+          backgroundColor: Colors.orangeAccent);
     }
   }
 }
