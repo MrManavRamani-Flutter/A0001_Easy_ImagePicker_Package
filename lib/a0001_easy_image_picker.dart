24197f8d56cd731bdcd0afb3d7971ec60f233aeb
manav <manavpatel13402003@gmail.com>
2024-05-22 08:00:25 +0530

    change BLoC code for Image Picker add lib...

--- lib/a0001_easy_image_picker.dart
+++ lib/a0001_easy_image_picker.dart
@@ -3,6 +3,8 @@
 library a0001_easy_image_picker;
 
 export 'src/bloc_image_picker/image_picker_bloc.dart';
+export 'src/bloc_image_picker/image_picker_event.dart';
+export 'src/bloc_image_picker/image_picker_state.dart';
 export 'src/getx_image_picker/image_picker_controller.dart';
 export 'src/provider_image_picker/image_model.dart';
 export 'src/provider_image_picker/image_picker_provider.dart';
